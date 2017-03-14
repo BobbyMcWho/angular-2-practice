11e3ec73fb967d69f9de61d4280aed0ed84371a9
Robert McDonald <contactrmcdonald@gmail.com>
2017-03-14 16:46:13 -0400

    continued work on tutorial

--- angular2-getting-started/APM/app/products/product-list.component.ts
+++ angular2-getting-started/APM/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core'
+import {IProduct} from './product'
 
 @Component({
     selector: 'pm-products',
@@ -7,7 +8,11 @@ import {Component} from '@angular/core'
 })
 export class ProductListComponent{
 pageTitle: string = 'Product List';
-products: any[] =[{
+imageWidth: number = 50;
+imageMargin: number = 2;
+showImage: boolean = false;
+listFilter: string = 'cart';
+products: IProduct[] =[{
         "productId": 1,
         "productName": "Leaf Rake",
         "productCode": "GDN-0011",
@@ -26,5 +31,9 @@ products: any[] =[{
         "price": 32.99,
         "starRating": 4.2,
         "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-    }]
+    }];
+
+    toggleImage(): void 
+    {this. showImage = !this.showImage;
+    }
 }
